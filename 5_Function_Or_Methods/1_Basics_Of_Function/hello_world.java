3179c226faad81dda96472d7fd2b059846f1d737
pawan0108 <pawanmaurya8931@gmail.com>
2024-02-29 22:09:01 +0530

    This is Function and Merthods Folder

---
+++ 5_Function_Or_Methods/1_Basics_Of_Function/hello_world.java
@@ -0,0 +1,10 @@
+public class hello_world {
+    public static void helloworld(){
+       System.out.println("Hello World");
+       System.out.println("Hello World");
+       System.out.println("Hello World");
+    }
+    public static void main(String[] args) {
+        helloworld();
+    }
+}
---
+++ 5_Function_Or_Methods/Print_Function_With_Parameter.java
@@ -0,0 +1,11 @@
+public class Print_Function_With_Parameter {
+    public static void Position(String name, int age){
+        System.out.println(name+ " Engineer & Age is "+age);
+    }
+
+    public static void main(String[] args) {
+        Position("Rahul", 21);
+        Position("Akash", 22);
+        Position("Mahesh", 23);
+    }
+}
